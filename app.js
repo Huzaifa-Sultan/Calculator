123aaa79bba13746c77ff1ac0cbf1c770f4b61ae
Huzaifa-Sultan <siddhuzaifa43@gmail.com>
2023-09-05 16:13:23 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,47 @@
+import inquirer from 'inquirer';
+const calculator = () => {
+    inquirer
+        .prompt([
+        {
+            type: 'input',
+            name: 'num1',
+            message: 'Enter the first number:',
+            validate: (value) => !isNaN(value) || 'Please enter a valid number.',
+        },
+        {
+            type: 'input',
+            name: 'num2',
+            message: 'Enter the second number:',
+            validate: (value) => !isNaN(value) || 'Please enter a valid number.',
+        },
+        {
+            type: 'list',
+            name: 'operation',
+            message: 'Select an operation:',
+            choices: ['Add', 'Subtract', 'Multiply', 'Divide'],
+        },
+    ])
+        .then((answers) => {
+        const num1 = parseFloat(answers.num1);
+        const num2 = parseFloat(answers.num2);
+        let result;
+        switch (answers.operation) {
+            case 'Add':
+                result = num1 + num2;
+                break;
+            case 'Subtract':
+                result = num1 - num2;
+                break;
+            case 'Multiply':
+                result = num1 * num2;
+                break;
+            case 'Divide':
+                result = num1 / num2;
+                break;
+            default:
+                result = 'Invalid operation';
+        }
+        console.log(`Result: ${result}`);
+    });
+};
+calculator();
